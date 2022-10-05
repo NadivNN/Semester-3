b8c02cb5e580c60a59aa3f4f18223e73d041ba86
Nadiv N <nadiv.n.ft21@mail.umy.ac.id>
2022-10-05 20:43:33 +0700

    creat method 3

--- Semester 3/Program.cs
+++ Semester 3/Program.cs
@@ -54,5 +54,23 @@ namespace Semester_3
             }
             Console.WriteLine("");
         }
+        public void BubbleSortArray()
+        {
+            for (int i = 1; i < n; i++) // For n - 1passes
+            {
+                //Pada pas i, bandingkan n - i elemen pertama dengan elemen selanjutnya
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (a[j] < a[j + 1]) // jika elemen tidak dalam urutan yang benar
+                    {
+                        // Tukar Elemen
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
     }
 }
